db2001477deb072e42ecf0e8d34b3e8a915baee3
ValeryManzo <valery.98@live.it>
2025-07-04 17:52:26 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const h1 = document.querySelector ('#titolo-principale');
+h1.innerText = 'Benvenuto nel mio sito';
+const p = document.querySelector ('#descrizione');
+p.innerText = 'Sito rinnovato con JavaScript';
+const button = document.querySelector('button');
+button.innerText = 'Inizia ora';
+
+
+const spesa = ['Pane', 'Latte', 'Uova', 'Pasta', 'Pomodori'];
+const ul = document.querySelector ('#spesa');
+spesa.forEach(prodotto => {
+    ul.innerHTML += `<li>${prodotto}</li>`;
+});
+
+//ul.innerText = ['Pane', 'Latte', 'Uova', 'Pasta', 'Pomodori'];
